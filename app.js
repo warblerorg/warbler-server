47475d5102746be6e8b01299104dadef898c5d66
Em Sal <absolutezero2a03@gmail.com>
2019-02-17 21:24:00 -0800

    everything so far (post/create requests untested)

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool();
+
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => res.send("Hello world!"));
+
+app.get('/v1/comment/:id', async (req, res, next) => {
+    try {
+        const queryResult = await pool.query("SELECT * FROM comments WHERE comment_id=$1", [req.params["id"]]);
+        if (queryResult.rows.length == 0) {
+            res.status(404).send({
+                "status": "error",
+                "type": "comment not found"
+            });
+        } else {
+            res.json(queryResult.rows[0]);
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).send("Internal error.");
+    }
+});
+app.post('/v1/comment', async (req, res, next) => {
+    try {
+        const queryResult = await pool.query("INSERT INTO comments(thread_id, parent_id, user_id, content), VALUES($1, $2, $3, $4)", 
+            [
+                req.body["thread_id"],
+                req.body["parent_id"],
+                req.body["user_id"],
+                req.body["content"]
+            ]
+        );
+        res.json(queryResult.rows[0]);
+    } catch(err) {
+        console.log(err);
+        res.status(500).send("Internal error inserting.");
+    }
+});
+//app.put('/v1/comment')
+//app.delete('/v1/comment/:id')
+
+//app.post('/v1/login')
+
+app.get('/v1/thread/:id', async (req, res, next) => {
+    try {
+        const queryResult = await pool.query("SELECT * FROM threads WHERE id=$1", [req.params["id"]]);
+        if (queryResult.rows.length == 0) {
+            res.status(404).json({
+                "status": "error",
+                "type": "thread not found"
+            });
+        } else {
+            res.json(queryResult.rows[0]);
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).send("Internal error.");
+    }
+});
+//app.post('/v1/thread')
+//app.put('/v1/thread')
+//app.delete('/v1/thread/:id')
+
+app.get('/v1/user/:id', async (req, res, next) => {
+    try {
+        const queryResult = await pool.query("SELECT * FROM users WHERE email_or_id=$1", [req.params["id"]]);
+        if (queryResult.rows.length == 0) {
+            res.status(404).json({
+                "status": "error",
+                "type": "user profile not found"
+            });
+        } else {
+            res.json(queryResult.rows[0]);
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).send("Internal error.");
+    }
+});
+//app.post('/v1/user')
+//app.put('/v1/user')
+//app.delete('/v1/user/:id')
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}.`));
---
+++ db_v1.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS threads;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE users(
+    email_or_id VARCHAR(512) PRIMARY KEY,
+    display_name VARCHAR(512),
+    website VARCHAR(512)
+);
+
+
+CREATE TABLE threads(
+    id VARCHAR(1024) PRIMARY KEY
+);
+
+CREATE TABLE comments(
+    thread_id VARCHAR(1024) REFERENCES threads(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    comment_id SERIAL,
+    parent_id INTEGER REFERENCES comments(comment_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    user_id VARCHAR(512) REFERENCES users(email_or_id),
+    content TEXT,
+    PRIMARY KEY (comment_id)
+);
